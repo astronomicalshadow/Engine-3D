1b26bd3fc1a0eead03b553d655846dd62d293d29
shadow <tdkuang@gmail.com>
2022-10-21 16:59:39 -0600

    find way to delete static ptr

--- src/3D.cpp
+++ src/3D.cpp
@@ -5,14 +5,14 @@
 int main()
 {
     // create window
-    sf::RenderWindow* window = getWindow({1280, 720}, "MyWindow"); // this is a horrible implementation, need to fix
+    sf::RenderWindow* s_Window = getWindow({1280, 720}, "MyWindow"); // this is a horrible implementation, need to fix
 
     // framerate cap, affects speed of rotation
-    window->setFramerateLimit(60);
+    s_Window->setFramerateLimit(60);
 
     // view 
     sf::View Camera(sf::Vector2f(0, 0), sf::Vector2f(89, 50)); // 1.7777:1 ratio (1280/720 = 1.7777)
-    window->setView(Camera);
+    s_Window->setView(Camera);
 
     // math engine
     Engine::E3D E3D(60 /*FOV*/);
@@ -30,17 +30,17 @@ int main()
 	Cube1->translate({ 1, 2, 0 }); // translates rotation of each object, z axis broken
 	E3D.translate(Cube1, { 5, 0, 0 }); // rotation at the center
 
-    while (window->isOpen())
+    while (s_Window->isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
 
-        while (window->pollEvent(event))
+        while (s_Window->pollEvent(event))
         {
             // "close requested" event: close the window
             if (event.type == sf::Event::Closed)
             {
-                window->close();
+                s_Window->close();
             }
         }
 
@@ -55,15 +55,18 @@ int main()
         Cube2->set_2D_Coordinates(E3D.applyPerspective(Cube2));
 
         // clear screen for update
-        window->clear(sf::Color::White);
+        s_Window->clear(sf::Color::White);
 
         // draw
         Cube1->DrawCuboid();
         Cube2->DrawCuboid();
 
         // display everything
-        window->display();
+        s_Window->display();
     }
+
+    // free static mem
+    // delete s_Window; <- MEM LEAK of static object. Inherit sf::drawable
 
     return 0;
 }
